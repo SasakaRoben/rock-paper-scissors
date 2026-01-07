ab845f901ceadaab714b6451c4bb3a97d2cb38fe
SasakaRoben <sasakarobenld@gmail.com>
2026-01-07 12:14:38 +0300

    Write logic to play 5 rounds

--- script.js
+++ script.js
@@ -17,3 +17,32 @@ function getHumanChoice() {
     return prompt("Enter your choice: ")
 }
 
+function playGame(humanChoice, computerChoice) {
+
+
+    if (humanChoice == "rock" && computerChoice == "scissors" ||
+        humanChoice == "paper" && computerChoice == "rock" ||
+        humanChoice == "scissors" && computerChoice == "paper") {
+        humanScore++;
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+    } else if (humanChoice == computerChoice) {
+        console.log(`It's a tie! You both chose ${humanChoice}.`);
+    } else {
+        computerScore++;
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+let humanChoice = getHumanChoice().toLowerCase();
+let computerChoice = getComputerChoice();
+
+for (let round = 1; round <= 5; round++) {
+    console.log(`Round ${round}:`);
+    playGame(humanChoice, computerChoice);
+    humanChoice = getHumanChoice().toLowerCase();
+    computerChoice = getComputerChoice();
+}
+
