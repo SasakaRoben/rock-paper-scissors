afa5d3698c55f7bec57752a62c4705e525fb9773
SasakaRoben <sasakarobenld@gmail.com>
2026-01-07 11:49:41 +0300

    Write logic to get computer choice

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello, World!");
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function getComputerChoice() 
+{
+    randomInt = getRandomInt(3);
+    if (randomInt === 0) {
+        return "rock";
+    } else if (randomInt === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
