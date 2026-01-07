0534f1f06d087f71ef1b08878b2bb18d01b5641e
SasakaRoben <sasakarobenld@gmail.com>
2026-01-07 11:53:57 +0300

    Get human choice

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-function getComputerChoice() 
-{
-    randomInt = getRandomInt(3);
+function getComputerChoice() {
+    let randomInt = getRandomInt(3);
     if (randomInt === 0) {
         return "rock";
     } else if (randomInt === 1) {
@@ -13,3 +12,8 @@ function getComputerChoice()
         return "scissors";
     }
 }
+
+function getHumanChoice() {
+    return prompt("Enter your choice: ")
+}
+
